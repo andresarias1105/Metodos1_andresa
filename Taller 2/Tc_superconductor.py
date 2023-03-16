1aa0717bbf63b4b6090e1f4c0374a59ffddd24f2
andresarias1105 <123577190+andresarias1105@users.noreply.github.com>
2023-03-15 21:54:07 -0500

    Tc final

--- Taller 2/Tc_superconductor.py
+++ Taller 2/Tc_superconductor.py
@@ -12,7 +12,7 @@ def funcion(x,T,deltaT):
        
 
 
-Roots,Weights=np.polynomial.legendre.leggauss(20)
+Roots,Weights=np.polynomial.legendre.leggauss(50)
 
 def Integrate(Rootsi,Weightsi,f,a,b,T,deltaT=0):
     t=0.5*((b-a)*Rootsi+a+b)
@@ -22,9 +22,9 @@ def Integrate(Rootsi,Weightsi,f,a,b,T,deltaT=0):
     return integral
 
 
-def Critical_temp(rango):
+def Critical_temp(rango,paso):
     
-    Tn=np.linspace(1,rango,100000)
+    Tn=np.arange(1,rango,paso)
     N0V=0.3
     for i in range(len(Tn)):
       Tc=Tn[i]
@@ -32,14 +32,14 @@ def Critical_temp(rango):
       I=Integrate(Roots,Weights,funcion,-1,1,Tc)
       
       
-      if  np.abs(I-(1/(N0V))) < 1e-5: 
+      if  np.abs(I-(1/(N0V))) < paso: 
          return Tc
       
         
-Tc=Critical_temp(21)   
+Tc=Critical_temp(20,1e-4)   
 I=Integrate(Roots,Weights,funcion,-1,1,Tc,0)
 Int=integrate.fixed_quad(funcion,-1,1,args=(Tc,0),n=20)
-print(Tc)
+print("la temperatura critica es aproximadamente: ",round(Tc,4))
 
 
   
